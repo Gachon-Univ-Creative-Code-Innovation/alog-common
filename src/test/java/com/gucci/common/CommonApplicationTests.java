e38b4f5170c74726b3523ac050b1b1c353cefb30
jeonjunbae <wnsqowid0901@gmail.com>
2025-04-03 12:36:16 +0900

    chore: 초기 셋팅

---
+++ src/test/java/com/gucci/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucci.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
